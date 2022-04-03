b6b1fb9f684781a1d9d2f939bb3705b938a51fc7
Satish Madawal <satishbm045@gmail.com>
2022-04-03 15:34:01 +0530

    Update postListDataReducer.js

--- src/redux/reducer/postListDataReducer.js
+++ src/redux/reducer/postListDataReducer.js
@@ -3,7 +3,7 @@ const defaultData = []
 export const postListDataReducer = (postData = defaultData, action) => {
     switch(action.type){
         case "ADD_ALL_POST":
-			var tempDataArray = postData;
+			var tempDataArray = [...postData];
 			tempDataArray = action.data ? action.data : postData;
 			tempDataArray.map((ele,index)=>{
 				ele.index = index
@@ -18,6 +18,15 @@ export const postListDataReducer = (postData = defaultData, action) => {
 				tempDataArray[action.data.index] = { ...tempDataArray[action.data.index], likes: tempDataArray[action.data.index].likes+1 };
 			}else if(tempDataArray[action.data.index].isLiked == false){
 				tempDataArray[action.data.index] = { ...tempDataArray[action.data.index], likes: tempDataArray[action.data.index].likes-1 };
+			}
+			return tempDataArray;
+
+		case "SORT_POST":
+			var tempDataArray = [...postData];
+			if(action.data){
+				tempDataArray.sort((a,b) => (a.likes > b.likes) ? 1 : ((b.likes > a.likes) ? -1 : 0))
+			}else{
+				tempDataArray.sort((a,b) => (a.index > b.index) ? 1 : ((b.index > a.index) ? -1 : 0))
 			}
 			return tempDataArray;
 		
