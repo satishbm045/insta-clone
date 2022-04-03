d2cf42ab4500a2a09e2fa664dcdef882f4338834
Satish Madawal <satishbm045@gmail.com>
2022-04-03 15:33:00 +0530

    Update ImageList.js

--- src/components/ImageList/ImageList.js
+++ src/components/ImageList/ImageList.js
@@ -1,18 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { useSelector, useDispatch } from 'react-redux';
 import './ImageList.css';
 import blackLike from '../../assets/blackLike.png';
 import redLike from '../../assets/redLike.png';
-import {slectedPost} from '../../redux/action/action';
+import {slectedPost, sortThePost} from '../../redux/action/action';
 
 const ImageList = () => {
+	const [sortCheckBox, setSortCheckBox] = useState(false);
 	const dispatch = useDispatch();
-	let imgData = useSelector(
-		(state) => 
-		state && state.postListDataReducer  ? state.postListDataReducer : []);
-	console.log(imgData);
+
+	let imgData = useSelector((state) => state && state.postListDataReducer  ? state.postListDataReducer : []);
+
+	const checkBoxOnChange = (e) => {
+		setSortCheckBox(e.target.checked);
+		dispatch(sortThePost(e.target.checked));
+	}
+	
 	return (
 		<div className="imageList">
+			<div className="data">
+				<div className="postHeading">Posts</div>
+				<div>Sort by likes <input className="checkBox" type={"checkbox"} value={sortCheckBox} onChange={(e)=>checkBoxOnChange(e)}/></div>
+			</div>
 			{imgData && imgData.length > 0 && imgData.map((ele,index)=> {
 				return (
 					<div className="eachImageBox" key={index}>
